79ca9c26132b315bee4145ffb2bf78e6a65418d0
markdly <markdly@users.noreply.github.com>
2018-05-10 16:24:57 +1000

    fix itanal test warnings and errors

--- tests/testthat/test-cq_itanal.R
+++ tests/testthat/test-cq_itanal.R
@@ -20,14 +20,15 @@ test_that("import zero item", {
 })
 
 test_that("item with one response code", {
-  x <- cq_itanal(system.file("extdata", "edge_case", "one_resp_code.itn", package = "conquestr"))
+  x <- suppressWarnings(cq_itanal(system.file("extdata", "edge_case", "one_resp_code.itn", package = "conquestr")))
+  expect_warning(cq_itanal(system.file("extdata", "edge_case", "one_resp_code.itn", package = "conquestr")))
   expect_equal(nrow(x), 1)
   expect_equal(trimws(x$id), "item:25 (one_resp_code)")
 })
 
 test_that("very difficult item with extreme logit", {
   x <- suppressWarnings(cq_itanal(system.file("extdata", "edge_case", "difficult_item.itn", package = "conquestr")))
-  expect_warning(cq_itanal(system.file("extdata", "edge_case", "minus32_item.itn", package = "conquestr")))
+  expect_warning(cq_itanal(system.file("extdata", "edge_case", "difficult_item.itn", package = "conquestr")))
   expect_equal(nrow(x), 1)
   expect_equal(trimws(x$id), "item:111 (difficult_item)")
   expect_equal(x$delta, -37.01)
