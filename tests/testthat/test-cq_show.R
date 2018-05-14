e77675936d99517dec9b1c56235bc140a031a168
markdly <markdly@users.noreply.github.com>
2018-05-14 13:57:04 +1000

    cq_show with long or no labels. added tests

--- tests/testthat/test-cq_show.R
+++ tests/testthat/test-cq_show.R
@@ -13,3 +13,15 @@ test_that("imports even if mnsq is not requested (fit=no)", {
   expect_equal(df1$est, x$est)
   expect_equal(df1$err, x$err)
 })
+
+test_that("long labels can be imported", {
+  x <- cq_show(system.file("extdata", "long_labels.shw", package = "conquestr"))
+  expect_equal(df1$est, x$est, tolerance = 0.01)
+})
+
+test_that("no labels are OK", {
+  x <- cq_show(system.file("extdata", "no_labels.shw", package = "conquestr"))
+  expect_equal(x$index, x$label)
+  expect_equal(x$label, seq.int(9))
+  expect_equal(df1$est, x$est, tolerance = 0.01)
+})
