6d647d3f99bb2cf17f8a61247f8040e91532fb6c
Mark Dulhunty <dulhunty@edmeasurement.com.au>
2018-09-12 15:42:10 +1000

    import all tables in show files

--- tests/testthat/test-cq_show.R
+++ tests/testthat/test-cq_show.R
@@ -1,6 +1,7 @@
 context("Show files")
 
 df1 <- cq_show(cq_example(display = FALSE, example_name = "ex1.shw"))
+df1 <- df1$data[[1]]
 df2 <- cq_itanal(cq_example(display = FALSE)) # using the itanal for comparison
 
 test_that("Values imported correctly", {
@@ -9,6 +10,7 @@ test_that("Values imported correctly", {
 
 test_that("imports even if mnsq is not requested (fit=no)", {
   x <-   cq_show(system.file("extdata", "no_fit.shw", package = "conquestr"), fit = FALSE)
+  x <- x$data[[1]]
   expect_warning(cq_show(system.file("extdata", "no_fit.shw", package = "conquestr")))
   expect_equal(df1$est, x$est)
   expect_equal(df1$err, x$err)
@@ -16,12 +18,22 @@ test_that("imports even if mnsq is not requested (fit=no)", {
 
 test_that("long labels can be imported", {
   x <- cq_show(system.file("extdata", "long_labels.shw", package = "conquestr"))
+  x <- x$data[[1]]
   expect_equal(df1$est, x$est, tolerance = 0.01)
 })
 
 test_that("no labels are OK", {
   x <- cq_show(system.file("extdata", "no_labels.shw", package = "conquestr"))
-  expect_equal(x$index, x$label)
-  expect_equal(x$label, seq.int(9))
+  x <- x$data[[1]]
+  expect_equal(x$item_index, x$item)
+  expect_equal(x$item, seq.int(9))
   expect_equal(df1$est, x$est, tolerance = 0.01)
 })
+
+test_that("interaction terms are imported", {
+  x <- cq_show(system.file("extdata", "dif.shw", package = "conquestr"))
+  expect_equal(x$term[3], "item*grp")
+  expect_equal(head(names(x$data[[3]]), 4),
+               c("item_index", "item", "grp_index", "grp"))
+})
+
